94458fdf4364145726cc9e0c7e8cde3ac422049d
desarrollocml <cristianmirandalamas@gmail.com>
2022-01-25 01:41:39 -0300

    consulta de pueba, mensaje

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 import express from "express";
 const app = express();
 
-app.listen(3000, () => console.log(`server on port 3000`));
+import { graphqlHTTP } from "express-graphql";
 
+import bSchema from "graphql";//CommonJS module
+const { buildSchema } = bSchema;
+
+//Schema de GraphQl
+const schema = buildSchema(`
+    type Query {
+        message: String
+    }
+`);
+
+const root = {
+  message: () => "Hola Mundo 2",
+};
+//endpoint de graphql
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+
+app.listen(3000, () => console.log(`server on port 3000`));
